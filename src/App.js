b19686a835ba5044c214fc0d9c593eaf9597dbea
Christian R <cristianrm11@gmail.com>
2021-06-06 12:33:15 -0600

    separate search into functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,10 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((m) =>
@@ -27,13 +32,10 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          type='search'
-          placeholder='search monsters'
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value });
-          }}
-        ></input>
+        <SearchBox
+          placeHolder='Search monsters'
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
