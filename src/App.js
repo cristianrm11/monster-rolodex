5b9db3678ff61a29cbda31e14bf1b662de33385b
Christian R <cristianrm11@gmail.com>
2021-06-06 12:00:36 -0600

    asplit into components, add some css and update readme with documentation lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -20,9 +21,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h2 key={monster.id}>{monster.name}</h2>
-        ))}
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h2 key={monster.id}>{monster.name}</h2>
+          ))}
+        </CardList>
       </div>
     );
   }
---
+++ src/components/card/card.component.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+import './card.styles.css';
