83c2e4f87550238f946ac26caffcedc74a7eebb3
Christian R <cristianrm11@gmail.com>
2021-06-06 12:10:42 -0600

    sadd Card component to render inside CardList component

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
 import './card.styles.css';
+
+export const Card = ({ monster }) => {
+  return (
+    <div className='card-container'>
+      <h2>{monster.name}</h2>
+    </div>
+  );
+};
